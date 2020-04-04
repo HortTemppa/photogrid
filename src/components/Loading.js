6c527c58d82f019f12827659a3c06f91596b362e
HortTemppa <temelameni@gmail.com>
2020-04-04 09:50:30 +0300

    some CSS tweaks

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "react-loader-spinner";
 
 const Loading = () => {
-  return (
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 300);
+
+  return loading ? (
     <div className="SinglePhotoContainer">
       <Loader type="Oval" color="black" height={80} width={80} />
     </div>
-  );
+  ) : null;
 };
 export default Loading;
--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -1,29 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 import axios from "axios";
 import Loading from "./Loading";
 
 const SingleImage = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   const [photo, setPhoto] = useState();
 
   useEffect(() => {
     axios
       .get(`https://jsonplaceholder.typicode.com/photos?id=${id}`)
-      .then(response => {
+      .then((response) => {
         setPhoto(response.data[0]);
       });
   }, [id]);
 
+  const handleXClick = () => {
+    history.push("/");
+  };
+
   console.log(id);
 
   return photo ? (
-    <div className="SinglePhotoContainer">
-      <h2>{photo.title}</h2>
-      <img className="GridItem" alt="" src={photo.url}></img>
-    </div>
+    <>
+      <button className="BackButton" onClick={handleXClick}>
+        X
+      </button>
+      <div className="SinglePhotoContainer">
+        <h2>{photo.title}</h2>
+        <img
+          className="GridItem"
+          onClick={handleXClick}
+          alt=""
+          src={photo.url}
+        ></img>
+      </div>
+    </>
   ) : (
     <Loading />
   );
